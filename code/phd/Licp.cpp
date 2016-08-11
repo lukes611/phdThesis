2002eb7364d1011070ebc4a5ca9eb14d976a8988
luke lincoln <lukes611@gmail.com>
2016-08-11 13:55:51 +1000

    icp almost done

--- code/phd/Licp.cpp
+++ code/phd/Licp.cpp
@@ -1,5 +1,6 @@
 #include "Licp.h"
-
+#include "../basics/LTimer.h"
+#include "../basics/Pixel3DSet.h"
 using namespace ll_R3;
 
 namespace Licp
@@ -19,4 +20,200 @@ Mat asMatRows(vector<R3> & inp)
 	return ret.clone();
 }
 
+float knn(Mat& m_destinations, Mat& m_object, vector<int>& ptpairs, vector<float>& dists)
+{
+    // find nearest neighbors using FLANN
+    cv::Mat m_indices(m_object.rows, 1, CV_32S);
+    cv::Mat m_dists(m_object.rows, 1, CV_32F);
+ 
+    Mat dest_32f; m_destinations.convertTo(dest_32f,CV_32FC2);
+    Mat obj_32f; m_object.convertTo(obj_32f,CV_32FC2);
+ 
+    assert(dest_32f.type() == CV_32F);
+ 
+    cv::flann::Index flann_index(dest_32f, cv::flann::KDTreeIndexParams(2));  // using 2 randomized kdtrees
+    flann_index.knnSearch(obj_32f, m_indices, m_dists, 1, cv::flann::SearchParams(64) ); 
+ 
+    int* indices_ptr = m_indices.ptr<int>(0);
+    //float* dists_ptr = m_dists.ptr<float>(0);
+    for (int i=0;i<m_indices.rows;++i) {
+        ptpairs.push_back(indices_ptr[i]);
+    }
+ 
+    dists.resize(m_dists.rows);
+    m_dists.copyTo(Mat(dists));
+ 
+    return cv::sum(m_dists)[0];
 }
+
+double closestPoints(vector<R3> & src, vector<R3> & dst, vector<int> & indexes, vector<double> & distances)
+{
+	//clear the output
+	indexes.clear(); distances.clear(); 
+	double averageDistance = 0.0f;
+	
+	if(dst.size() <= 0 || src.size() <= 0) return averageDistance; //if dst has no elements: return
+	double scalar = 1.0 / (double) src.size(); //scalar for computing the averageDistane
+	for(int i = 0; i < src.size(); i++) //for each of the src input
+	{
+		R3 srcPoint = src[i];
+		double bestDistance = (double)srcPoint.dist(dst[0]);
+		int bestIndex = 0;
+		for(int j = 1; j < dst.size(); j++) //search for the closest point in dst
+		{
+			double dist = (double)srcPoint.dist(dst[j]);
+			if(dist < bestDistance)
+			{
+				bestIndex = j;
+				bestDistance = dist;
+			}
+		}
+		distances.push_back(bestDistance);
+		indexes.push_back(bestIndex);
+		averageDistance += bestDistance * scalar;
+	}
+	return averageDistance;
+}
+
+double closestPointsf(vector<R3> & src, vector<R3> & dst, vector<int> & indexes, vector<double> & distances)
+{
+	distances.clear();
+	indexes.clear();
+	Mat srcm = asMatRows(src), dstm = asMatRows(dst);
+	vector<float> dists;
+	double error = (double) knn(dstm, srcm, indexes, dists);
+	for(int i = 0; i < dists.size(); i++) distances.push_back((double)dists[i]);
+	double divisor = src.size() > 0 ? (double)(src.size()) : 1.0;
+	return error / divisor;
+}
+
+
+Mat leastSquaresTransform(vector<R3> & src, vector<R3> & dst, vector<int> & indexes)
+{
+	//M x src = dst
+	Mat ret;
+	Mat src_m = Mat::zeros(4, indexes.size(), CV_32FC1);
+	Mat dst_m = Mat::zeros(4, indexes.size(), CV_32FC1);
+	for(int i = 0; i < indexes.size(); i++)
+	{
+		src_m.at<float>(0, i) = src[i].x;
+		src_m.at<float>(1, i) = src[i].y;
+		src_m.at<float>(2, i) = src[i].z;
+		src_m.at<float>(3, i) = 1.0f;
+
+		int j = indexes[i];
+
+		dst_m.at<float>(0, i) = dst[j].x;
+		dst_m.at<float>(1, i) = dst[j].y;
+		dst_m.at<float>(2, i) = dst[j].z;
+		dst_m.at<float>(3, i) = 1.0f;
+	}
+	//src^T x M^T = dst^T
+	src_m = src_m.t();
+	dst_m = dst_m.t();
+	Mat Mt = least_squares(src_m, dst_m);
+	Mat M = Mt.t();
+	return M.clone();
+}
+
+Mat icp(vector<R3> & src, vector<R3> & dst, vector<R3> & out, 
+		double & error_out, double & time, int & iterations, 
+		double minError, int maxIterations)
+{
+	time = 0.0;
+	LTimer clock; clock.start();
+	out.clear();
+	Mat ret = Mat::eye(Size(4,4), CV_32FC1);
+	vector<double> dl;
+	vector<int> ind;
+	out = src;
+	iterations = 0;
+	
+	double best_distance = DBL_MAX;
+	error_out = best_distance;
+	
+	Mat T = ret.clone();
+
+	while(true)
+	{
+		dl.clear(); ind.clear();
+		double current_distance = closestPointsf(out, dst, ind, dl);
+		if(current_distance >= best_distance) break; //cannot do any better
+		ret = T * ret;
+		error_out = current_distance;
+		if(minError > 0.0 && current_distance < minError) break;
+		if(maxIterations >= 0 && iterations >= maxIterations) break;
+		best_distance = current_distance;
+		T = leastSquaresTransform(out, dst, ind);
+		for(int i = 0; i < out.size(); i++) ll_pix3d::Pixel3DSet::transform_point(T, out[i]);
+		iterations++;
+	}
+	clock.stop();
+	time = clock.getSeconds();
+	return ret.clone();
+}
+
+
+Mat icp_outlierRemoval(vector<R3> & src, vector<R3> & _dst, vector<R3> & out, 
+					   double & error_out, double & time, int & iterations, 
+					   double outlierRemovalTimesMean, double minError, 
+					   int maxIterations)
+{
+	time = 0.0;
+	LTimer clock; clock.start();
+	out.clear();
+	Mat ret = Mat::eye(Size(4,4), CV_32FC1);
+	vector<double> dl;
+	vector<int> ind;
+	out = src;
+	vector<R3> dst = _dst;
+	iterations = 0;
+	
+	double best_distance = DBL_MAX;
+	error_out = best_distance;
+	
+	Mat T = ret.clone();
+
+	while(true)
+	{
+		dl.clear(); ind.clear();
+		double current_distance = Licp::closestPointsf(out, dst, ind, dl);
+		if(current_distance >= best_distance) break; //cannot do any better
+		ret = T * ret;
+		error_out = current_distance;
+		if(minError > 0.0 && current_distance < minError) break;
+		if(maxIterations >= 0 && iterations >= maxIterations) break;
+		best_distance = current_distance;
+		if(best_distance <= 0.0) break;
+
+		//outlier removal
+		//cout << current_distance << " is current, or is: " << outlierRemovalTimesMean << endl;
+		vector<R3> srcTest, dstTest;
+		vector<int> indTest;
+		for(int i = 0; i < dl.size(); i++)
+		{
+			double test = dl[i] / best_distance;
+			//cout << dl[i] << " : " << best_distance << " : " << test << " <= " << outlierRemovalTimesMean << endl;
+			if(dl[i]/best_distance < outlierRemovalTimesMean)
+			{
+				srcTest.push_back(out[i]);
+				dstTest.push_back(dst[ind[i]]);
+				indTest.push_back(i);
+			}
+		}
+		
+		//cout << "i/o(" << out.size() << "/" << srcTest.size() << ")\n";
+
+		T = Licp::leastSquaresTransform(srcTest, dstTest, indTest);
+		for(int i = 0; i < out.size(); i++) ll_pix3d::Pixel3DSet::transform_point(T, out[i]);
+		iterations++;
+	}
+	clock.stop();
+	time = clock.getSeconds();
+	return ret.clone();
+}
+
+
+
+}
+
--- code/phd/Licp.h
+++ code/phd/Licp.h
@@ -5,9 +5,17 @@
 implementation of the iterative closest point algorithm
 author : luke lincoln @ lukes611@gmail.com
 
-requires: 
+requires: locv, R3
 
 */
+
+//a n^2 complexity search for the closest point for each point in src to dst,
+//returns an array of mappings src[i] mapped to dst[array[i]]
+// ipc wiki: https://en.wikipedia.org/wiki/Iterative_closest_point
+// kind of good: http://www.mrpt.org/Iterative_Closest_Point_(ICP)_and_other_matching_algorithms
+// //optimal resource: http://www.morethantechnical.com/2010/06/06/iterative-closest-point-icp-with-opencv-w-code/
+// : least squares: https://en.wikipedia.org/wiki/Linear_least_squares_(mathematics)
+
 
 #include "../basics/locv3.h"
 #include "../basics/R3.h"
@@ -27,6 +35,57 @@ namespace Licp
 		 .    .  .   .
 		[xn, yn, zn, 1]
 	*/
-	Mat asMatRows(vector<ll_R3::R3> & inp); 
+	Mat asMatRows(vector<ll_R3::R3> & inp);
+
+	//for each point in src: find the corresponding nearest neighbour in dst
+	//returns a float representing the average distance from each closest point from src to dst
+	//returns ptpairs: where src[i] matches with dst[ptpairs[i]] as output of the matching
+	//dists represents the distance between src[i] and its match dst[ptpairs[i]]
+	float knn(Mat & dst, Mat & src, vector<int> & ptpairs, vector<float> & dists = vector<float>());
+
+
+	//finds the closest point for each point in src to a point in dst
+	//returns a float representing the average distance from each closest point from src to dst
+	//returns indexes: where src[i] matches with dst[indexes[i]] as output of the matching
+	//distances represents the distance between src[i] and its match dst[ptpairs[i]]
+	//algorithm : O(n^2) basic search
+	double closestPoints(vector<ll_R3::R3> & src, vector<ll_R3::R3> & dst, vector<int> & indexes, vector<double> & distances);
+
+	//finds the closest point for each point in src to a point in dst speedily
+	//returns a float representing the average distance from each closest point from src to dst
+	//returns indexes: where src[i] matches with dst[indexes[i]] as output of the matching
+	//distances represents the distance between src[i] and its match dst[ptpairs[i]]
+	//algorithm : O(n log n) k-d tree based algorithm
+	double closestPointsf(vector<ll_R3::R3> & src, vector<ll_R3::R3> & dst, vector<int> & indexes, vector<double> & distances);
+
+	//performs least squares to find the transform from all the points in src to all the points in dst
+	//here: src[i] is matched with dst[indexes[i]] for a given index i, 
+	Mat leastSquaresTransform(vector<ll_R3::R3> & src, vector<ll_R3::R3> & dst, vector<int> & indexes);
+
+
+	//implementation of the iterative cloesest point algorithm
+	//does not remove outliers
+	Mat icp(	vector<ll_R3::R3> & src,		//source points
+				vector<ll_R3::R3> & dst,		//destination points
+				vector<ll_R3::R3> & out,		//registered points
+				double & error_out,		//error between registered points and destination points
+				double & time,			//the amount of time in seconds required to run this algorithm
+				int & iterations,		//the number of iterations used
+				double minError = -1.0, //the minimum error we want to achieve
+				int maxIterations = -1  //the maximum number of iterations
+			);
+
+	//implementation of the iterative cloesest point algorithm
+	//remove's outliers
+	Mat icp_outlierRemoval(	vector<ll_R3::R3> & src,			//source points
+				vector<ll_R3::R3> & dst,						//destination points
+				vector<ll_R3::R3> & out,						//registered points
+				double & error_out,								//error between registered points and destination points
+				double & time,									//the amount of time in seconds required to run this algorithm
+				int & iterations,								//the number of iterations used
+				double outlierRemovalTimesMean,					//a value: matches are not outliers if: |match| <= ortm * |average|
+				double minError = -1.0,							//the minimum error we want to achieve
+				int maxIterations = -1							//the maximum number of iterations
+			);
 
 }
