5b4291a397f55fc0eb9dc0288ed0f765e5e7ce52
luke lincoln <lukes611@gmail.com>
2017-07-04 00:30:21 +1000

    did some more experiments

--- Source.cpp
+++ Source.cpp
@@ -444,18 +444,18 @@ int main(int argc, char * * argv)
 
 	//for(int i = 13; i < 20; i++)
 	{
-	string fn = namesList[3];
+	string fn = namesList[6];
 	//exp1("Apartment.Texture.rotate", ll_experiments::rng(15, 20, 1));
 
-    //quantitativeExperiment20("none", fn, inds);
-    //quantitativeExperiment20("FM2D", fn, inds);
-    //quantitativeExperiment20("FM3D", fn, inds);
-    //quantitativeExperiment20("ICP", fn, inds);
-    //quantitativeExperiment20("PCA", fn, inds);
-    //quantitativeExperiment20("FVR", fn, inds);
-    //quantitativeExperiment20("FVR3D", fn, inds);
-    //quantitativeExperiment20("FVR3D-2", fn, inds);
-	//quantitativeExperiment20("FFVR", fn, inds);
+    quantitativeExperiment20("none", fn, inds);
+    quantitativeExperiment20("FM2D", fn, inds);
+    quantitativeExperiment20("FM3D", fn, inds);
+    quantitativeExperiment20("ICP", fn, inds);
+    quantitativeExperiment20("PCA", fn, inds);
+    quantitativeExperiment20("FVR", fn, inds);
+    quantitativeExperiment20("FVR3D", fn, inds);
+    quantitativeExperiment20("FVR3D-2", fn, inds);
+	quantitativeExperiment20("FFVR", fn, inds);
 
 
     }
@@ -492,7 +492,7 @@ int main(int argc, char * * argv)
     //quantitativeExperimentKitti10("FVR", kittiData, inds);
     //quantitativeExperimentKitti10("FVR3D", kittiData, inds);
     //quantitativeExperimentKitti10("FVR3D-2", kittiData, inds);
-	quantitativeExperimentKitti10("FFVR", kittiData, inds);
+	//quantitativeExperimentKitti10("FFVR", kittiData, inds);
 
 
 
