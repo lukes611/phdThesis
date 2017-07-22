7a68f45e62ab1b79b26d4c527b9cd41622df851b
lukes611 <lukes611@gmail.com>
2017-07-22 20:25:53 +1000

    adding more experiments and re-doing some stuff

--- code/phd/LCamExperiments.h
+++ code/phd/LCamExperiments.h
@@ -13,9 +13,16 @@
 //use w,a,s & d to find the translation. - to decrease the velocity, + to increase it and f - to finish the curration
 
 */
-void currate(ll_pix3d::Pix3D a, ll_pix3d::Pix3D b, std::string distance);
+//void currate(ll_pix3d::Pix3D a, ll_pix3d::Pix3D b, std::string data_set, std::string distance);
 
 //currate-2 eg. currate("trans.5cminc.office1", 0, 1, "5cm");
-void currate(std::string datasetname, int index1, int index2, std::string distance);
+//void currate(std::string datasetname, int index1, int index2, std::string distance);
+
+
+//algorithm - name, amount, translation - x, rotation, noise - level, SNR
+
 
 #endif
+
+void lcamExpT(std::string dataset, std::string algorithm);
+void lcamExpR(std::string dataset, std::string algorithm);
